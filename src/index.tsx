32196d991080e011c256cf38f9105aa29b173ea8
Yuta Hiroto <git@about-hiroppy.com>
2019-01-31 13:51:28 +0900

    fix: page -> Page

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { client } from './client';
-import { Top } from './components/page';
+import { Top } from './components/Page';
 
 const root = document.createElement('div');
 
